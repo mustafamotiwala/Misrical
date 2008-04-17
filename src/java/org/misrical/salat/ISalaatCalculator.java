9d260b54a8c62d7f92252b7528e32abd27790c8d
mustafa.motiwala <mustafa.motiwala@aa0661a6-8c49-0410-86b0-f50e59ead534>
2008-04-17 05:10:15 +0000

    Enhancements to the build script Added a couple util classes

--- src/java/org/misrical/salat/ISalaatCalculator.java
+++ src/java/org/misrical/salat/ISalaatCalculator.java
@@ -4,16 +4,19 @@ import java.util.Calendar;
 import java.util.Map;
 
 public interface ISalaatCalculator {
-  public ISalaatClock getSalatTime(Calendar date);
-  
-  /**
-   * This method returns a Map of salaat times for the given month in the given year. The
-   * key of the map is the day of the month.
-   * @param year - The Gregorian year (AD) 
-   * @param month - The month in the given year for which to calculate
-   * the salaat times.
-   * @return Returs a map of days of the given month and the corresponding
-   * salaat times object.
-   */
-  public Map<Integer, ISalaatClock> getSalatTimesForMonth(int year, int month);
+    public ISalaatClock getSalatTime(Calendar date);
+
+    /**
+     * This method returns a Map of salaat times for the given month in the
+     * given year. The key of the map is the day of the month.
+     * 
+     * @param year -
+     *            The Gregorian year (AD)
+     * @param month -
+     *            The month in the given year for which to calculate the salaat
+     *            times.
+     * @return Returs a map of days of the given month and the corresponding
+     *         salaat times object.
+     */
+    public Map<Integer, ISalaatClock> getSalatTimesForMonth(int year, int month);
 }
